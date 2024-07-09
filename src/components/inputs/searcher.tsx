dbf87849c06861ca851327e4755c8e76818284e5
marioVillamar <mjvc98@hotmail.es>
2024-07-09 10:40:50 -0500

    refactor search result

--- src/components/inputs/searcher.tsx
+++ src/components/inputs/searcher.tsx
@@ -3,21 +3,47 @@ import { useEffect, useRef, useState } from 'react'
 import { useDebounceValue } from 'usehooks-ts'
 import { useAction } from '@/hooks/use-action'
 import { getSearch } from '@/actions/shared/get-search'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList
-} from '../ui/command'
 import Loader from '../loader'
 import { cn } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
+import { Input } from '../ui/input'
 
 
 interface SearcherProps {
   isBusiness?: boolean
+}
+
+const ResultGroup = ({
+  heading,
+  children
+}: {
+  heading: string,
+  children: React.ReactNode
+}) => {
+  return (
+    <div className=''>
+      <h4 className='text-xs text-gray-700 py-1'>{heading}</h4>
+      <div className='pl-1'>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+const ResultItem = ({
+  onClick,
+  children,
+  className
+}: {
+  onClick: () => void,
+  children: React.ReactNode,
+  className?: string
+}) => {
+  return (
+    <div role='button' onClick={onClick} className={cn('cursor-pointer text-xs py-1 hover:bg-gray-100 duration-200', className)}>
+      {children}
+    </div>
+  )
 }
 
 const Searcher = ({
@@ -49,34 +75,33 @@ const Searcher = ({
   }, [debouncedValue])
 
   return (
-    <Command className='relative overflow-visible'>
-      <CommandInput
+    <div className='relative overflow-visible'>
+      <Input
         className='w-full' placeholder='Buscar'
-        onValueChange={(value) => setValue(value)}
+        onChange={(e) => setValue(e.target.value)}
         onFocus={() => setOpen(true)}
         onBlur={() => setTimeout(() => setOpen(false), 200)}
       />
       {
         open && (
-          <CommandList className={cn(
+          <div className={cn(
             'absolute top-14 z-40 w-80 sm:w-[500px] md:w-80 lg:w-96 bg-popover p-4 text-popover-foreground rounded-md shadow-md outline-none',
             data && 'pt-0'
           )}>
             {isLoading && <div className='w-full flex justify-center'><Loader className='w-5 h-5' /></div>}
-            {!isLoading && !data && <CommandEmpty className='p-0 text-center text-xs'>Busca una empresa o estudiante!</CommandEmpty>}
+            {!isLoading && !data && <div className='p-0 text-center text-xs'>Busca una empresa o estudiante!</div>}
             {
               data && !isLoading && data.students.length > 0 && (
-                <CommandGroup heading='Estudiantes'>
+                <ResultGroup heading='Estudiantes' >
                   {
                     data.students.map((student) => (
-                      <CommandItem key={student.id}
-                        value={student.studentId}
-                        onSelect={(value) => {
+                      <ResultItem key={student.id}
+                        onClick={() => {
                           const startRoute = isBusiness ? '/b/profile/e' : '/e/profile'
-                          router.push(`${startRoute}/${value}`)
+                          router.push(`${startRoute}/${student.studentId}`)
                           setOpen(false)
                         }}
-                        className='cursor-pointer text-xs'
+                        className=''
                       >
                         <p className='overflow-hidden whitespace-nowrap text-ellipsis'>
                           <span className='text-sm text-black'>{student.name}</span>
@@ -89,27 +114,26 @@ const Searcher = ({
                             )
                           }
                         </p>
-                      </CommandItem>
+                      </ResultItem>
                     ))
                   }
-                </CommandGroup>
+                </ResultGroup>
               )
             }
             {
               data && !isLoading && data.business.length > 0 && (
-                <CommandGroup heading='Empresas'>
+                <ResultGroup heading='Empresas' >
                   {
                     data.business.map((business) => {
                       console.log(business);
                       return (
-                        <CommandItem key={business.id}
-                          value={business.businessId}
-                          onSelect={(value) => {
+                        <ResultItem key={business.id}
+                          onClick={() => {
                             const startRoute = isBusiness ? '/b/profile' : '/e/profile/b'
-                            router.push(`${startRoute}/${value}`)
+                            router.push(`${startRoute}/${business.businessId}`)
                             setOpen(false)
                           }}
-                          className='cursor-pointer text-xs'
+                          className=''
                         >
                           <p className='overflow-hidden whitespace-nowrap text-ellipsis'>
                             <span className='text-sm text-black'>{business.name}</span>
@@ -122,17 +146,17 @@ const Searcher = ({
                               )
                             }
                           </p>
-                        </CommandItem>
+                        </ResultItem>
                       )
                     })
                   }
-                </CommandGroup>
+                </ResultGroup>
               )
             }
-          </CommandList>
+          </div>
         )
       }
-    </Command>
+    </div>
   )
 }
 
